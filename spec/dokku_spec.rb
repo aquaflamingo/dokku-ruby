44a4a074337d4494be66928d486249e633a86b08
robertsimoes <robertsimoes@users.noreply.github.com>
2020-08-14 20:58:42 -0700

    [Specs] Updates dokku_spec and removes DSL

--- spec/dokku_spec.rb
+++ spec/dokku_spec.rb
@@ -3,9 +3,19 @@
 RSpec.describe Dokku do
   describe '.start_session!' do
     context 'without user set' do
+      it 'raises missing user error' do
+        expect { Dokku.start_session! }.to raise_error(Dokku::MissingUserError)
+      end
     end
 
     context 'without host set' do
+      before do
+        Dokku.config { |c| c.user = '123' }
+      end
+
+      it 'raises missing host error' do
+        expect { Dokku.start_session! }.to raise_error(Dokku::MissingHostError)
+      end
     end
   end
 end
